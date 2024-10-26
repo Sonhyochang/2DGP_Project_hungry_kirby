644e93e21329ab2c209b8da08240ac9909af33ec
Hyochang-Son@main notebook <dagdf20@naver.com>
2024-10-26 19:05:05 +0900

    커비 점프 방향(좌) 구현

--- hungry_kirby.py
+++ hungry_kirby.py
@@ -1,6 +1,8 @@
 from pico2d import*
 import random
 
+from pygame.display import is_fullscreen
+
 
 class Kirby:
     def __init__(self):
@@ -8,6 +10,7 @@ class Kirby:
         self.frame = 0
         self.dir = 0
         self.dir2 = 0
+        self.dir3 =0
         self.action = 9
         self.jump = False
         self.high = False
@@ -33,7 +36,10 @@ class Kirby:
             elif event.key == SDLK_LEFT:
                 self.dir -= 1
             elif event.key == SDLK_UP:
-                self.dir2 = 1
+                if self.dir < 0:
+                    self.dir2 = -1
+                else:
+                    self.dir2 = 1
                 self.jump = True
                 self.high = True
             elif event.key == SDLK_SPACE:
@@ -87,12 +93,15 @@ class Kirby:
         elif not self.jump and self.dir < 0:
             self.image.clip_composite_draw(79 + self.frame * 23, self.action * 34, 23, 34, 0, 'h', self.x, self.y, 50, 50)
 
-        if self.jump and self.dir2 == 0:
-            if self.space_jump:
-                self.image.clip_draw(474 + self.frame * 30 ,self.action * 34 - 34, 30, 34, self.x, self.y,50,50)
-            else:
-                self.image.clip_draw(644 + self.frame * 25, self.action * 34, 24, 34, self.x, self.y, 50, 50)
-        elif self.dir2 > 0:
+        if self.jump:
+            if self.dir2 < 0:
+                self.image.clip_composite_draw(644 + self.frame * 25, self.action * 34, 24, 34, 0, 'h', self.x, self.y,50, 50)
+            elif self.dir2 == 0:
+                if self.space_jump:
+                    self.image.clip_draw(474 + self.frame * 30 ,self.action * 34 - 34, 30, 34, self.x, self.y,50,50)
+                else:
+                    self.image.clip_draw(644 + self.frame * 25, self.action * 34, 24, 34, self.x, self.y, 50, 50)
+            elif self.dir2 > 0:
                 self.image.clip_draw(644 + self.frame * 25, self.action * 34, 24, 34, self.x, self.y, 50, 50)
 
 
